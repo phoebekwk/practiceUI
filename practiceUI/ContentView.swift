0c0712baefb892683af59a7b505fbc6743302423
Phoebe <phoebe_macmillan@icloud.com>
2024-07-12 14:14:23 -0400

    Initial Commit

---
+++ practiceUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  practiceUI
+//
+//  Created by Stuart Macmillan on 2024-07-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ practiceUI/practiceUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceUIApp.swift
+//  practiceUI
+//
+//  Created by Stuart Macmillan on 2024-07-12.
+//
+
+import SwiftUI
+
+@main
+struct practiceUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
